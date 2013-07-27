d0147e333a365fff23b866c5ce2b4d6da26af57d
Ral <contato@raloliver.com>
2013-07-27 15:47:48 -0300

    S1 - Bootstrap
    Uso da biblioteca BOOTSTRAP na prática.

---
+++ README.md
@@ -0,0 +1,3 @@
+s1
+==
+Uso da biblioteca BOOTSTRAP na prática.
---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+		<head>
+			<title>S1 - StartOne</title>
+			<meta name="viewport" content="width=device-width,initial-scale=1.0">
+			<meta charset="utf-8">
+			<!-- Bootstrap -->
+			<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+			<!--[if lt IE 9]>
+				<script $src="js/html5shiv.js"></script>
+			<![endif]-->
+			<style type="text/css">
+				.container{background: red;}
+				.span3{background: green;}
+				.span4{background: orange;}
+			</style>
+		</head>
+		<body>
+			<div class="container">
+				<div class="row">
+					<div class="span3">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, deleniti expedita nemo voluptatibus deserunt. Voluptates eum minima quibusdam esse est laudantium saepe iusto reprehenderit error totam. Mollitia amet dicta aspernatur.
+					</div>
+					<div class="span4 offset1">
+						<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, atque, iure, repellat pariatur assumenda vel quo in sint consectetur quis consequuntur veritatis autem porro voluptatibus praesentium beatae asperiores mollitia ex.</p>
+					</div>				
+					<div class="span3 offset1">
+						<abbr title="Nome Completo">Nome:</abbr>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, deleniti expedita nemo voluptatibus deserunt. Voluptates eum minima quibusdam esse est laudantium saepe iusto reprehenderit error totam. Mollitia amet dicta aspernatur.
+					</div>					
+				</div>
+			<div class="row">
+				<div class="span6 text-right">
+					<address>
+					<strong>Twitter, Inc.</strong><br>
+					Av. Paulista, 00-00<br>
+					São Paulo, SP - CEP: 09732-00<br>
+					<abbr title="Telefone">Tel:</abbr> (11) 0000-0000
+					</address>
+				</div>
+				<div class="span6">
+					<address>
+					<strong>Nome completo</strong><br>
+					<a href="mailto:#">nome.sobrenome@gmail.com</a>
+					</address>
+				</div>
+			</div>
+			<div class="row">
+				<div class="span6">
+					<blockquote class="pull-right">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, modi, minus, ex consequatur reiciendis ratione autem aperiam cum totam eveniet dignissimos fugit magni repudiandae reprehenderit perspiciatis consequuntur architecto ipsam quam.
+							<small>
+								Israel <cite>g1.com</cite>
+							</small>
+						</p>
+					</blockquote>
+				</div>
+				<div class="span6">
+					<code>Coração</code>
+					<pre>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, ex aliquid natus temporibus iste praesentium obcaecati dolor consequatur quo doloremque quae fuga voluptatum at odit earum omnis sapiente veritatis amet.
+					</pre>
+				</div>
+			</div>
+			</div>
+				<div class="span12">
+					<table class="table table-bordered table-hover">
+						<caption>Tabela 1</caption>
+							<thead>
+								<tr>
+									<th>Coluna 1</th>
+									<th>Coluna 2</th>
+									<th>Coluna 3</th>
+								</tr>
+							</thead>
+						<tbody>
+							<tr class="error">
+								<td>Linha 1</td>
+								<td>Linha 1</td>
+								<td>Linha 1</td>
+							</tr>
+							<tr class="info">
+								<td>Linha 2</td>
+								<td>Linha 2</td>
+								<td>Linha 2</td>
+							</tr>
+							<tr class="success">
+								<td>Linha 3</td>
+								<td>Linha 3</td>
+								<td>Linha 3</td>
+							</tr>
+							<tr class="warning">
+								<td>Linha 3</td>
+								<td>Linha 3</td>
+								<td>Linha 3</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+
+			<!-- Javascript -->
+			<script src="http://code.jquery.com/jquery.js"></script>
+			<script src="js/bootstrap.min.js"></script>
+		</body>
+</html>
