fd628e5d28e90112f39701ae5032606934d42410
Ral <contato@raloliver.com>
2013-09-28 15:57:18 -0300

    LESS
    Exemplo básico do uso de LESS com operadores

---
+++ less.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>LESS</title>
+	<link rel="stylesheet/less" type="text/css" href="css/template.less" />
+	<script src="js/less.js" type="text/javascript"></script>
+</head>
+<body>
+	<h1>Lorem ipsum dolor sit amet</h1>
+	<h2>Consectetur adipisicing elit. Repudiandae, officiis, nulla ad suscipit laborum repellendus velit voluptates nisi odit maxime excepturi ipsa voluptatem fugiat similique error ratione doloribus itaque corporis.</h2>
+</body>
+</html>
